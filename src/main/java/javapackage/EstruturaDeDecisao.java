418652495e9b4d6ee6266a8ac35cfefa78c6b3d6
Caio Silva <csm14103@gmail.com>
2024-05-28 11:47:03 -0300

    Firste commit

---
+++ src/main/java/javapackage/EstruturaDeDecisao.java
@@ -0,0 +1,12 @@
+package javapackage;
+
+public class EstruturaDeDecisao {
+    public static void main(String[] args) {
+        Integer numero = 145;
+        if (numero % 2 == 0){
+            System.out.println("verdadeiro, é par");
+        }else{
+            System.out.println("falso, é ímpar");
+        }
+    }
+}
